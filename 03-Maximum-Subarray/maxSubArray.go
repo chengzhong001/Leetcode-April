15348aa164b37af16a05d2655b096a1096a07ad8
azhong <zczwzw@163.com>
2022-05-23 21:28:11 +0800

    update

--- 03-Maximum-Subarray/maxSubArray.go
+++ 03-Maximum-Subarray/maxSubArray.go
@@ -2,35 +2,35 @@ package main
 
 import "fmt"
 
-func maxSubArray(nums []int) int {
-	max := nums[0]
-	sum := 0
-	for _, n := range nums {
-		if sum < 0 {
-			sum = n
-		} else {
-			sum += n
-		}
-		if sum > max {
-			max = sum
-		}
-	}
-	return max
-}
-
 // func maxSubArray(nums []int) int {
 // 	max := nums[0]
-// 	for i := range nums {
-// 		sum := 0
-// 		for _, v := range nums[i:] {
-// 			sum += v
-// 			if sum > max {
-// 				max = sum
-// 			}
+// 	sum := 0
+// 	for _, n := range nums {
+// 		if sum < 0 {
+// 			sum = n
+// 		} else {
+// 			sum += n
+// 		}
+// 		if sum > max {
+// 			max = sum
 // 		}
 // 	}
 // 	return max
 // }
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := range nums {
+		sum := 0
+		for _, v := range nums[i:] {
+			sum += v
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+	return max
+}
 
 func main() {
 	// nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
