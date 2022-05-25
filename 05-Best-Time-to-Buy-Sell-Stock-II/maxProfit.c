cc404fc064a6f4f3ff0decb528b59007aaadd4e8
azhong <zczwzw@163.com>
2022-05-25 22:07:04 +0800

    sell stock update

--- 05-Best-Time-to-Buy-Sell-Stock-II/maxProfit.c
+++ 05-Best-Time-to-Buy-Sell-Stock-II/maxProfit.c
@@ -1,23 +1,64 @@
 #include <stdio.h>
 
+// int maxProfit(int *prices, int pricesSize)
+// {
+//     if (pricesSize <= 1)
+//         return 0;
+
+//     int max = 0;
+//     int profit;
+//     profit = maxProfit(prices, pricesSize - 1);
+//     if (profit > max)
+//         max = profit;
+
+//     for (int i = 1; i <= pricesSize - 1; i++)
+//     {
+//         profit = maxProfit(prices, i) + prices[pricesSize - 1] - prices[i - 1];
+//         if (profit > max)
+//             max = profit;
+//     }
+//     return max;
+// }
+
+// int maxProfit(int *prices, int pricesSize){
+//     int total = 0;
+//     for (int i = 0; i +1< pricesSize; i++)
+//     {
+//         if(prices[i] < prices[i+1])
+//             total += prices[i+1] - prices[i];
+//     }
+//     return total;
+    
+// }
+
 int maxProfit(int *prices, int pricesSize)
 {
     if (pricesSize <= 1)
         return 0;
 
-    int max = 0;
-    int profit;
-    profit = maxProfit(prices, pricesSize - 1);
-    if (profit > max)
-        max = profit;
+    int profits[pricesSize + 1];
 
-    for (int i = 1; i <= pricesSize - 1; i++)
+    profits[1] = 0;
+
+    for (int k = 2; k <= pricesSize; k++)
     {
-        profit = maxProfit(prices, i) + prices[pricesSize - 1] - prices[i - 1];
+        int profit;
+        int max = 0;
+        profit = profits[k - 1];
+
         if (profit > max)
+        {
             max = profit;
+        }
+        for (int i = 1; i <= k - 1; i++)
+        {
+            profit = profits[i] + prices[k - 1] - prices[i - 1];
+            if (profit > max)
+                max = profit;
+        }
+        profits[k] = max;
     }
-    return max;
+    return profits[pricesSize];
 }
 
 int main(int argc, char const *argv[])
