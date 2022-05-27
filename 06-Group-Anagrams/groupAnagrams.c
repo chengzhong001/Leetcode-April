458475ebfe8970d48b701e0e25687a4b289e9cb8
azhong <zczwzw@163.com>
2022-05-27 22:59:48 +0800

    group anagrams problem

---
+++ 06-Group-Anagrams/groupAnagrams.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char ***groupAnagrams(char **strs, int strSize, int *returnSize, int **returnColumnsizes)
+{
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char s[12] = "hello world";
+    char *strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+    const char *p = &s[0];
+    printf("%s\n", p);
+    printf("%s\n", strs);
+    return 0;
+}
