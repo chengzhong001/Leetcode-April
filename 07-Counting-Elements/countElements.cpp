2e574d1dba7183cfca4b9b9a1811130271cd7c23
azhong <zczwzw@163.com>
2022-05-29 00:02:38 +0800

    count Elements c++

--- 07-Counting-Elements/countElements.cpp
+++ 07-Counting-Elements/countElements.cpp
@@ -1,9 +1,25 @@
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 int countElements(std::vector<int> arr)
 {
+    std::sort(arr.begin(), arr.end());
+    int count = 0;
+    int number = 1;
     
-    return 0;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if(arr[i] != arr[i-1]){
+            if(arr[i]==arr[i-1]+1)
+                count += number;
+            number = 1;
+        }
+        else{
+            number++;
+        }
+    }
+    return count;
 }
 
 int main(int argc, char const *argv[])
@@ -11,12 +27,12 @@ int main(int argc, char const *argv[])
     /* code */
     std::vector<int> arr = {1, 2, 3};
     int res = countElements(arr);
-
+    std::cout << "countElements: " << res << std::endl;
     arr = {1, 1, 3, 3, 5, 5, 7, 7};
     res = countElements(arr);
-
+    std::cout << "countElements: " << res << std::endl;
     arr = {1, 3, 2, 3, 5, 0};
-    int res2 = countElements(arr);
-
+    res = countElements(arr);
+    std::cout << "countElements: " << res << std::endl;
     return 0;
 }
